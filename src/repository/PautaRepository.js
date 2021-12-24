8cb1b06f78026603c8b6d55be43b8a5eb47e0542
felipefahrion <felipefahrion@MacBook-Pro.local>
2021-12-24 15:28:01 -0300

    refactor on PautaRepository class

--- src/repository/PautaRepository.js
+++ src/repository/PautaRepository.js
@@ -1,13 +1,14 @@
 class PautaRepository {
     constructor(){
-        this.pautas = [{
-            idPauta: "1",
-            descricao: 'TESTE',
-            tempoCriacao: "21/12/2021",
-            tempoFim: "22/12/2021",
-            status: 'open',
-            votos: []
-        }]
+        // this.pautas = [{
+        //     idPauta: "1",
+        //     descricao: 'TESTE',
+        //     tempoCriacao: "21/12/2021",
+        //     tempoFim: "22/12/2021",
+        //     status: 'open',
+        //     votos: []
+        // }]
+        this.pautas = []
     }
 }
 
